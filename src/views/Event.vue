1e593f6a4261ef545fcf43aa7b5a7668125f1c9e
Thomas Zhu <thomas.zhu.sh@hotmail.com>
2023-04-30 21:06:58 +0100

    update event

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -4,7 +4,11 @@
       <span class="index">#{{ $route.params.id }}</span>
       {{ event.name }}
     </h1>
-    <span>Organizer: <span class="address">{{ event.organizer }}</span></span>
+    <p>Organizer: <span class="address">{{ event.organizer }}</span></p>
+
+    <div @click="join" v-if="!isParticipant" style="cursor: pointer;" class="card card-hover">+ Join as Team Captain</div>
+    <div @click="addMember" v-if="isCaptain" style="cursor: pointer;" class="card card-hover">+ Add a Team Member</div>
+    <div @click="sponsor" style="cursor: pointer;" class="card card-hover">+ Sponsor an Award</div>
 
     <h2>Teams</h2>
     <div v-for="team in teams" class="card">
@@ -17,40 +21,64 @@
 </template>
 
 <script>
-import { view_event, view_team_num, view_team, view_award_num, view_award } from '@/SCinteraction.js';
+import { view_event, view_team_num, view_team, view_award_num, view_award, view_members_num, view_member } from '@/SCinteraction.js';
+import { joinEvent, sponsor, selectWinner, addMember, claimAward } from '@/SCinteraction.js';
 
 export default {
   name: 'Event',
   data() {
     return {
+      id: 0,
       event: {},
       awards: [],
       teams: [],
-      loading: true
+      loading: true,
+
+      // TODO
+      isParticipant: false,
+      isCaptain: false
     };
   },
   async created() {
-    const id = this.$route.params.id;
-    this.event = await view_event(id);
+    this.id = this.$route.params.id;
+    this.event = await view_event(this.id);
     console.log(this.event);
 
-    const numTeams = (await view_team_num(id)).toNumber();
+    const numTeams = (await view_team_num(this.id)).toNumber();
     this.teams = [];
     for (let i = 0; i < numTeams; i++) {
-      const team = await view_team(id, i);
+      const team = await view_team(this.id, i);
+      team.members = [];
+      const numMembers = (await view_members_num(this.id, i)).toNumber();
+      for (let j = 0; j < numMembers; j++) {
+        const member = await view_member(this.id, i, j);
+        //
+        team.members.push(member);
+      }
       console.log('team', team);
       this.teams.push(team);
     }
 
-    const numAwards = (await view_award_num(id)).toNumber();
+    const numAwards = (await view_award_num(this.id)).toNumber();
     this.awards = [];
     for (let i = 0; i < numAwards; i++) {
-      const award = await view_award(id, i);
+      const award = await view_award(this.id, i);
       console.log('award', award);
       this.awards.push(award);
     }
     
     this.loading = false;
+  },
+  methods: {
+    async join() {
+      await joinEvent(this.id, /* TODO */ 'team123');
+    },
+    async sponsor() {
+      // TODO
+    },
+    async addMember() {
+      // TODO
+    }
   }
 }
 </script>
